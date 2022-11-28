7a9669d7be9d8dc843f9386b6ceb3bc1d10d84a4
buuvo <buuvo95@gmail.com>
2022-11-28 14:27:04 +0700

    Create initial Project

--- src/test/java/guru/springframework/msscbeerservice/MsscBeerServiceApplicationTest.java
+++ src/test/java/guru/springframework/msscbeerservice/MsscBeerServiceApplicationTest.java
@@ -4,10 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MsscBeerServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+public class MsscBeerServiceApplicationTest {
+    @Test
+    void contextLoads() {
+    }
 }
