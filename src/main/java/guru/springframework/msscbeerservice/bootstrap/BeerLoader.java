536f1e7df7730ac6f6b6228e0cbb3833f7beec8c
Buu Vo <buuvo95@gmail.com>
2022-11-30 20:18:09 +0700

    fix bugs

--- src/main/java/guru/springframework/msscbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/guru/springframework/msscbeerservice/bootstrap/BeerLoader.java
@@ -3,15 +3,17 @@ package guru.springframework.msscbeerservice.bootstrap;
 import guru.springframework.msscbeerservice.domain.Beer;
 import guru.springframework.msscbeerservice.repository.BeerRepository;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 
+/**
+ * Created by jt on 2019-05-17.
+ */
 @Component
 public class BeerLoader implements CommandLineRunner {
 
-    private BeerRepository beerRepository;
+    private final BeerRepository beerRepository;
 
     public BeerLoader(BeerRepository beerRepository) {
         this.beerRepository = beerRepository;
@@ -19,29 +21,29 @@ public class BeerLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        leadBeerObjects();
+        loadBeerObjects();
     }
 
-    private void leadBeerObjects() {
-        if(beerRepository.count() == 0) {
-            beerRepository.save(Beer.builder()
-                            .beerName("Mango Bobs")
-                            .beerStyle("IPA")
-                            .quantityToBrew(200)
-                            .minOnHand(12)
-                            .upc(999L)
-                            .price(new BigDecimal("12.95"))
-                            .build());
+    private void loadBeerObjects() {
+        if(beerRepository.count() == 0){
 
             beerRepository.save(Beer.builder()
-                    .beerName("Galaxy Cat")
+                    .beerName("Mango Bobs")
                     .beerStyle("IPA")
                     .quantityToBrew(200)
                     .minOnHand(12)
-                    .upc(999L)
+                    .upc(337010000001L)
+                    .price(new BigDecimal("12.95"))
+                    .build());
+
+            beerRepository.save(Beer.builder()
+                    .beerName("Galaxy Cat")
+                    .beerStyle("PALE_ALE")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(337010000002L)
                     .price(new BigDecimal("11.95"))
                     .build());
         }
-        System.out.println("Loaded Beers: " + beerRepository.count());
     }
 }
